adf4a70fbc7fb76298c50033e746eadcc1fc7c3c
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-04-12 23:23:48 +0100

    Add a console window

--- src/pages/Dashboard/components/Console.js
+++ src/pages/Dashboard/components/Console.js
@@ -5,11 +5,21 @@ import React, {PureComponent} from 'react';
 class Console extends PureComponent {
     render() {
         return (
-            <div id='console'>
+            <div contentEditable>
 
             </div>
         )
     }
 }
 
-export default Console
+class ConsoleWindow extends PureComponent {
+    render() {
+        return (
+            <div id='console-window'>
+                <Console />
+            </div>
+        )
+    }
+}
+
+export default ConsoleWindow
